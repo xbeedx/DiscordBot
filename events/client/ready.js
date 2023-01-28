81fffc6321be51d4e3f50879e8ccdd78ec7993df
Buja9 <105441709+smiyou@users.noreply.github.com>
2023-01-28 16:59:32 +0100

    runSlash commands

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,12 @@
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log('I\'m ready Boss');
+
+        //ID server
+        const devGuild = await client.guilds.cache.get(process.env.SERVER_ID);
+        //set / commands to server
+        devGuild.commands.set(client.commands.map(cmd => cmd));
     }
-}
+};
